d29fd20154964e976c5c7243ae0be564c021bc4a
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2021-05-01 12:43:33 -0400

    modify zsh script

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,19 +1,6 @@
 #!/bin/sh
 
 sh_c='sh -c'
-if [ "$user" != 'root' ]; then
-	if command_exists sudo; then
-		sh_c='sudo -E sh -c'
-	elif command_exists su; then
-		sh_c='su -c'
-	else
-		cat >&2 <<-'EOF'
-		Error: this installer needs the ability to run commands as root.
-		We are unable to find either "sudo" or "su" available to make this happen.
-		EOF
-		exit 1
-	fi
-fi
 
 get_distribution() {
 	lsb_dist=""
@@ -40,14 +27,7 @@ case "$lsb_dist" in
     ;;
 	
 	centos|fedora|rhel)
-		if [ "$lsb_dist" = "fedora" ]; then
-			pkg_manager="dnf"
-		else
-			pkg_manager="yum"
-		fi
-		(
-			$sh_c "$pkg_manager install -y -q zsh"
-		)
+		sudo yum install -y -q zsh
 	;;
 			
 esac
